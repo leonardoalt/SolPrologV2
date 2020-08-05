6250d0ff32dcea8c0e6296adcf54b07e20e80990
Kamil Śliwak <kamil.sliwak@codepoets.it>
2020-08-05 14:51:17 +0200

    Add TermBuilder

--- src/Logic.sol
+++ src/Logic.sol
@@ -34,3 +34,29 @@ library Logic {
 		return keccak256(abi.encodePacked(_term.kind, _term.symbol, args));
 	}
 }
+
+contract TermBuilder {
+	function term(bytes memory _symbol) internal pure returns (Term memory) {
+		Term memory t;
+		t.kind = TermKind.Predicate;
+		t.symbol = uint(keccak256(_symbol));
+		return t;
+	}
+
+	function term(bytes memory _symbol, uint _argumentCount) internal pure returns (Term memory) {
+		Term memory t = term(_symbol);
+		t.arguments = new Term[](_argumentCount);
+		return t;
+	}
+
+	function compare(Term storage _term1, Term memory _term2) internal view returns (bool) {
+		if (_term1.kind != _term2.kind || _term1.symbol != _term2.symbol || _term1.arguments.length != _term2.arguments.length)
+			return false;
+
+		for (uint i = 0; i < _term1.arguments.length; ++i)
+			if (!compare(_term1.arguments[i], _term2.arguments[i]))
+				return false;
+
+		return true;
+	}
+}
